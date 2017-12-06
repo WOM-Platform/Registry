7f2b8d254b4fb0ce2cd9adae38ef9f467cf32a9f
NikoBizzarri <34192989+NikoBizzarri@users.noreply.github.com>
2017-12-06 16:47:01 +0100

    Initial API controllers

---
+++ src/web-api/Controllers/VoucherController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WomPlatform.Web.Api.Models;
+
+namespace WomPlatform.Web.Api.Controllers
+{
+    [Route("voucher")]
+    public class VoucherController : Controller
+    {
+
+        [HttpPost("create")]
+        public CreateResponse Create([FromBody]CreatePayload payload)
+        {
+            //return string.Format("ID {0}", payload.SourceId);
+
+            return new CreateResponse
+            {
+                Id = Guid.NewGuid(),
+                OtcGen = "Prova"
+            };
+        }
+
+        [HttpPost("redeem")]
+        public void Redeem()
+        {
+
+        }
+
+    }
+}
---
+++ src/web-api/Models/CreateResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WomPlatform.Web.Api.Models
+{
+    public class CreateResponse
+    {
+        public Guid Id { get; set; }
+
+        public string OtcGen { get; set; }
+    }
+}
