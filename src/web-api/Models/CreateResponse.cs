8f6ba606041a5e7a9df7cea0f2f07928ce146ed6
NikoBizzarri <34192989+NikoBizzarri@users.noreply.github.com>
2017-12-06 18:50:16 +0100

    Aggiunta nuova classe e alcuni commenti
    Aggiunta la classe che gestisce le informazioni dei vari voucher

--- src/web-api/Models/CreateResponse.cs
+++ src/web-api/Models/CreateResponse.cs
@@ -10,5 +10,11 @@ namespace WomPlatform.Web.Api.Models
         public Guid Id { get; set; }
 
         public string OtcGen { get; set; }
+
+        public string Timestamp { get; set; }
+
+        //le informazioni relative ai voucher vengono gestite dalla classe VoucherInfo
+        public VoucherInfo[] nVoucher {get;set; }
+        
     }
 }
