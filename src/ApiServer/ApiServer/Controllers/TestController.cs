4c7c3c985cde6ed332b4e771755b29127ba01a85
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-12-03 21:42:13 +0100

    Clean up

--- src/ApiServer/ApiServer/Controllers/TestController.cs
+++ src/ApiServer/ApiServer/Controllers/TestController.cs
@@ -12,26 +12,31 @@ namespace ApiServer.Controllers {
     [Route("api/debug")]
     public class TestController : ControllerBase {
 
-        protected readonly IConfiguration _configuration;
-        protected readonly DatabaseManager _database;
-        protected readonly CryptoProvider _crypto;
-        protected readonly IHostingEnvironment _hosting;
-        protected readonly ILogger<TestController> _logger;
-
         protected readonly Random _rnd = new Random();
 
-        public TestController(IConfiguration configuration, DatabaseManager database, CryptoProvider cryptoProvider,
-            IHostingEnvironment hosting, ILogger<TestController> logger) {
-            this._configuration = configuration;
-            this._database = database;
-            this._crypto = cryptoProvider;
-            this._hosting = hosting;
-            this._logger = logger;
+        public TestController(
+            IConfiguration configuration,
+            DatabaseManager database,
+            CryptoProvider cryptoProvider,
+            IHostingEnvironment hosting,
+            ILogger<TestController> logger
+        ) {
+            Configuration = configuration;
+            Database = database;
+            Crypto = cryptoProvider;
+            Hosting = hosting;
+            Logger = logger;
         }
+
+        protected IConfiguration Configuration { get; }
+        protected DatabaseManager Database { get; }
+        protected CryptoProvider Crypto { get; }
+        protected IHostingEnvironment Hosting { get; }
+        protected ILogger<TestController> Logger { get; }
 
         [HttpPost("create-vouchers/{count=10}")]
         public IActionResult CreateVouchers([FromRoute]int count) {
-            if(!this._hosting.IsDevelopment()) {
+            if(!Hosting.IsDevelopment()) {
                 return this.Unauthorized();
             }
 
@@ -39,30 +44,30 @@ namespace ApiServer.Controllers {
                 throw new ArgumentOutOfRangeException(nameof(count), "Voucher count out of range");
             }
 
-            this._logger.LogInformation("Creating {0} test vouchers", count);
+            Logger.LogInformation("Creating {0} test vouchers", count);
 
-            var testSource = this._database.Connection.GetSourceById(1);
+            var testSource = Database.Connection.GetSourceById(1);
 
             var now = DateTime.UtcNow;
             var voucherInfos = new List<VoucherCreatePayload.VoucherInfo>();
             for(int i = 0; i < count; ++i) {
                 voucherInfos.Add(new VoucherCreatePayload.VoucherInfo {
-                    Latitude = this._rnd.NextBetween(5, 40),
-                    Longitude = this._rnd.NextBetween(5, 50),
+                    Latitude = _rnd.NextBetween(5, 40),
+                    Longitude = _rnd.NextBetween(5, 50),
                     Timestamp = now
                 });
 
                 now = now.Subtract(TimeSpan.FromMinutes(5));
             }
 
-            var result = this._database.Connection.CreateVoucherGeneration(new VoucherCreatePayload.Content {
+            var result = Database.Connection.CreateVoucherGeneration(new VoucherCreatePayload.Content {
                 SourceId = testSource.Id,
                 Vouchers = voucherInfos.ToArray()
             });
 
-            this._logger.LogTrace("New voucher generation request created with code {0}", result);
+            Logger.LogTrace("New voucher generation request created with code {0}", result);
 
-            return this.Content(result.ToString("N"));
+            return Content(result.ToString("N"));
         }
 
     }
--- src/ApiServer/ApiServer/Controllers/VoucherController.cs
+++ src/ApiServer/ApiServer/Controllers/VoucherController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Crypto;
-using WomPlatform.Web.Api.DatabaseModels;
 using WomPlatform.Web.Api.Models;
 
 namespace WomPlatform.Web.Api.Controllers {
@@ -19,11 +18,11 @@ namespace WomPlatform.Web.Api.Controllers {
             KeyManager keyManager,
             ILogger<VoucherController> logger)
         {
-            this.Configuration = configuration;
-            this.Database = databaseManager;
-            this.Crypto = cryptoProvider;
-            this.KeyManager = keyManager;
-            this.Logger = logger;
+            Configuration = configuration;
+            Database = databaseManager;
+            Crypto = cryptoProvider;
+            KeyManager = keyManager;
+            Logger = logger;
         }
 
         protected IConfiguration Configuration { get; }
@@ -35,29 +34,29 @@ namespace WomPlatform.Web.Api.Controllers {
         // POST api/v1/voucher/create
         [HttpPost("create")]
         public VoucherCreateResponse Create([FromBody]VoucherCreatePayload payload) {
-            this.Logger.LogInformation(LoggingEvents.VoucherCreation, "Received create request from Source ID {0}, nonce {1}",
+            Logger.LogInformation(LoggingEvents.VoucherCreation, "Received create request from Source ID {0}, nonce {1}",
                 payload.SourceId, payload.Nonce
             );
 
-            var source = this.Database.Connection.GetSourceById(payload.SourceId);
+            var source = Database.Connection.GetSourceById(payload.SourceId);
             if(source == null) {
-                this.Logger.LogError(LoggingEvents.VoucherCreation, "Source ID {0} does not exist", payload.SourceId);
+                Logger.LogError(LoggingEvents.VoucherCreation, "Source ID {0} does not exist", payload.SourceId);
                 // TODO: correct error handling
                 return null;
             }
 
             var sourcePublicKey = KeyManager.LoadKeyFromString<AsymmetricKeyParameter>(source.PublicKey);
 
-            var payloadContent = this.Crypto.DecryptAndVerify<VoucherCreatePayload.Content>(payload.Payload,
-                sourcePublicKey, this.KeyManager.RegistryPublicKey);
+            var payloadContent = Crypto.DecryptAndVerify<VoucherCreatePayload.Content>(payload.Payload,
+                sourcePublicKey, KeyManager.RegistryPublicKey);
 
             if(payload.SourceId != payloadContent.SourceId) {
-                this.Logger.LogError(LoggingEvents.VoucherCreation, "Verification failed, source ID {0} differs from ID {1} in payload", payload.SourceId, payloadContent.SourceId);
+                Logger.LogError(LoggingEvents.VoucherCreation, "Verification failed, source ID {0} differs from ID {1} in payload", payload.SourceId, payloadContent.SourceId);
                 // TODO
                 return null;
             }
             if(payload.Nonce != payloadContent.Nonce) {
-                this.Logger.LogError(LoggingEvents.VoucherCreation, "Verification failed, nonce {0} differs from nonce {1} in payload", payload.Nonce, payloadContent.Nonce);
+                Logger.LogError(LoggingEvents.VoucherCreation, "Verification failed, nonce {0} differs from nonce {1} in payload", payload.Nonce, payloadContent.Nonce);
                 // TODO
                 return null;
             }
@@ -65,14 +64,14 @@ namespace WomPlatform.Web.Api.Controllers {
 
             var nextNonce = "todo";
 
-            this.Logger.LogInformation(LoggingEvents.VoucherCreation, "Processing voucher generation for source {0}, nonce {1}", payload.SourceId, payload.Nonce);
+            Logger.LogInformation(LoggingEvents.VoucherCreation, "Processing voucher generation for source {0}, nonce {1}", payload.SourceId, payload.Nonce);
 
-            var otc = this.Database.Connection.CreateVoucherGeneration(payloadContent);
+            var otc = Database.Connection.CreateVoucherGeneration(payloadContent);
 
-            this.Logger.LogTrace(LoggingEvents.VoucherCreation, "Voucher generation instance created with OTC {0}", otc);
+            Logger.LogTrace(LoggingEvents.VoucherCreation, "Voucher generation instance created with OTC {0}", otc);
 
             return new VoucherCreateResponse {
-                Payload = this.Crypto.SignAndEncrypt(new VoucherCreateResponse.Content {
+                Payload = Crypto.SignAndEncrypt(new VoucherCreateResponse.Content {
                     Source = UrlGenerator.GenerateSourceUrl(source.Id),
                     NextNonce = nextNonce,
                     Otc = otc
@@ -83,16 +82,16 @@ namespace WomPlatform.Web.Api.Controllers {
         // POST api/v1/voucher/redeem/{OTC}
         [HttpPost("redeem/{otc}")]
         public ActionResult Redeem([FromRoute]string otc, [FromBody]VoucherRedeemPayload payload) {
-            this.Logger.LogInformation(LoggingEvents.VoucherRedemption, "Received redeem request with OTC {0}",
+            Logger.LogInformation(LoggingEvents.VoucherRedemption, "Received redeem request with OTC {0}",
                 otc
             );
 
             if(!Guid.TryParseExact(otc, "N", out Guid otcGen)) {
-                return this.NotFound();
+                return NotFound();
             }
 
             try {
-                var vouchers = this.Database.Connection.GenerateVouchers(otcGen);
+                var vouchers = Database.Connection.GenerateVouchers(otcGen);
                 var converted = (from v in vouchers
                                  select new VoucherRedeemResponse.VoucherInfo {
                                      Id = v.Id,
@@ -107,12 +106,12 @@ namespace WomPlatform.Web.Api.Controllers {
                     Vouchers = converted.ToArray()
                 };
 
-                return this.Ok(new VoucherRedeemResponse {
-                    Payload = this.Crypto.Sign(content, this.KeyManager.RegistryPrivateKey)
+                return Ok(new VoucherRedeemResponse {
+                    Payload = Crypto.Sign(content, KeyManager.RegistryPrivateKey)
                 });
             }
             catch(Exception ex) {
-                this.Logger.LogError(LoggingEvents.VoucherRedemption, ex, "Failed to generate vouchers");
+                Logger.LogError(LoggingEvents.VoucherRedemption, ex, "Failed to generate vouchers");
 
                 throw;
             }
