b37fba3ef87b5a4fa8cdffa9e91c745ef0507ea8
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-01-28 23:41:18 +0100

    Minor cleanup

--- src/ApiServer/ApiServer/Startup.cs
+++ src/ApiServer/ApiServer/Startup.cs
@@ -16,7 +16,6 @@ namespace WomPlatform.Web.Api {
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -28,13 +27,11 @@ namespace WomPlatform.Web.Api {
             services.AddScoped<DatabaseManager>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
-            // Web management
             app.UseMvc();
         }
     }
