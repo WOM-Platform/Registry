25f2b6ae68a30eb823664d515ee0518eae49c80d
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-02-08 18:52:17 +0100

    Add sample POS and keys

--- testkeys/certificate_generation.bat
+++ testkeys/certificate_generation.bat
@@ -1,1 +1,8 @@
+REM Certificate generation
 openssl req -new -x509 -key registry.pem -out registry.cer -nodes -days 365 -subj "/C=IT/ST=PU/L=Urbino/O=University of Urbino"
+
+REM Generate RSA private key (4096 bits)
+openssl genrsa -out pos1.pem 4096
+
+REM Generate RSA public key from private
+openssl rsa -in pos1.pem -pubout -out pos1.pem
