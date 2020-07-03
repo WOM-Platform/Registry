176bf8b498efaa642fe3d59215fe12ac247af0fe
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-07-03 12:48:10 +0200

    Add static page for WOM diAry

---
+++ src/ApiServer/ApiServer/Controllers/StaticController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WomPlatform.Web.Api.Controllers {
+
+    [Route("")]
+    public class StaticController : Controller {
+
+        protected ILogger<StaticController> Logger { get; }
+
+        public StaticController(
+            ILogger<StaticController> logger
+        ) {
+            Logger = logger;
+        }
+
+        [HttpGet("diary")]
+        public IActionResult ShowDiaryLandingPage() {
+            return View("Diary");
+        }
+
+    }
+
+}
