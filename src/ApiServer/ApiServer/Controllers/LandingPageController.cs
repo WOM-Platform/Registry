fd67002698e66da185d5143017cf0d80100ffc99
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2020-10-23 22:15:44 +0200

    Fix landing page

--- src/ApiServer/ApiServer/Controllers/LandingPageController.cs
+++ src/ApiServer/ApiServer/Controllers/LandingPageController.cs
@@ -1,37 +1,55 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using WomPlatform.Connector;
 using WomPlatform.Web.Api.ViewModel;
 
 namespace WomPlatform.Web.Api.Controllers {
 
     [Route("")]
-    public class LandingPageController : Controller {
-
-        protected ILogger<LandingPageController> Logger { get; }
+    public class LandingPageController : BaseRegistryController {
 
         public LandingPageController(
-            ILogger<LandingPageController> logger
-        ) {
-            Logger = logger;
+            IConfiguration configuration,
+            CryptoProvider crypto,
+            KeyManager keyManager,
+            MongoDatabase mongo,
+            Operator @operator,
+            ILogger<LandingPageController> logger)
+        : base(configuration, crypto, keyManager, mongo, @operator, logger) {
+
         }
 
         [HttpGet("payment/{otc}")]
-        public IActionResult ShowPaymentLandingPage(
+        public async Task<IActionResult> ShowPaymentLandingPage(
             [FromRoute] Guid otc
         ) {
-            return View("Payment", new LandingPageViewModel {
-                Otc = otc
-            });
+            var payment = await Mongo.GetPaymentRequestByOtc(otc);
+            if(payment == null) {
+                return View("NotFound");
+            }
+            else {
+                return View("Payment", new LandingPageViewModel {
+                    Otc = otc
+                });
+            }
         }
 
         [HttpGet("vouchers/{otc}")]
-        public IActionResult ShowGenerationLandingPage(
+        public async Task<IActionResult> ShowGenerationLandingPage(
             [FromRoute] Guid otc
         ) {
-            return View("Generation", new LandingPageViewModel {
-                Otc = otc
-            });
+            var generation = await Mongo.GetGenerationRequestByOtc(otc);
+            if(generation == null || generation.PerformedAt.HasValue || generation.Void) {
+                return View("NotFound");
+            }
+            else {
+                return View("Generation", new LandingPageViewModel {
+                    Otc = otc
+                });
+            }
         }
 
     }
