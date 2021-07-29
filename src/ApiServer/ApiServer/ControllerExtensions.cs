85771a7f8905f873af0ca6c503a2aa8eadb6ec28
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2021-07-29 16:23:15 +0200

    Update problem details output (use built-in method)

--- src/ApiServer/ApiServer/ControllerExtensions.cs
+++ src/ApiServer/ApiServer/ControllerExtensions.cs
@@ -9,66 +9,83 @@ namespace WomPlatform.Web.Api {
     public static class ControllerExtensions {
 
         public static ActionResult ProblemParameter(this ControllerBase c, string title = null) {
-            return new ProblemResult(
-                422, // Unprocessable Entity, see https://httpstatuses.com/422
-                "wrong-parameter",
-                title ?? "Request parameter not valid"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 422, // Unprocessable Entity, see https://httpstatuses.com/422
+                    Type = "wrong-parameter",
+                    Title = title ?? "Request parameter not valid"
+                }
             );
         }
 
         public static ActionResult RequestVoid(this ControllerBase c, string title = null) {
-            return new ProblemResult(
-                410, // Gone, see https://httpstatuses.com/410
-                "request-void",
-                title ?? "Request instance is void"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 410, // Gone, see https://httpstatuses.com/410
+                    Type = "request-void",
+                    Title = title ?? "Request instance is void"
+                }
             );
         }
 
         public static ActionResult UnexpectedError(this ControllerBase c, string title = null) {
-            return new ProblemResult(
-                500,
-                "unexpected",
-                title ?? "Unexpected internal error"
+
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 500,
+                    Type = "unexpected",
+                    Title = title ?? "Unexpected internal error"
+                }
             );
         }
 
         public static ActionResult SourceNotFound(this ControllerBase c) {
-            return new ProblemResult(
-                404,
-                "source-not-found",
-                "Source with the specified ID does not exist"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 404,
+                    Type = "source-not-found",
+                    Title = "Source with the specified ID does not exist"
+                }
             );
         }
 
         public static ActionResult PosNotFound(this ControllerBase c) {
-            return new ProblemResult(
-                404,
-                "pos-not-found",
-                "POS with the specified ID does not exist"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 404,
+                    Type = "pos-not-found",
+                    Title = "POS with the specified ID does not exist"
+                }
             );
         }
 
         public static ActionResult SourceVerificationFailure(this ControllerBase c) {
-            return new ProblemResult(
-                401,
-                "source-verification-failed",
-                "Failed to verifiy source identity"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 401,
+                    Type = "source-verification-failed",
+                    Title = "Failed to verifiy source identity"
+                }
             );
         }
 
         public static ActionResult PayloadVerificationFailure(this ControllerBase c, string title = null) {
-            return new ProblemResult(
-                403,
-                "payload-verification-failure",
-                title ?? "Failed to verify request contents"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 403,
+                    Type = "payload-verification-failure",
+                    Title = title ?? "Failed to verify request contents"
+                }
             );
         }
 
         public static ActionResult PasswordUnacceptableFailure(this ControllerBase c, string title = null) {
-            return new ProblemResult(
-                422, // Unprocessable Entity, see https://httpstatuses.com/422
-                "password-unacceptable",
-                title ?? "Supplied password is not acceptable"
+            return new ObjectResult(
+                new ProblemDetails {
+                    Status = 422, // Unprocessable Entity, see https://httpstatuses.com/422
+                    Type = "password-unacceptable",
+                    Title = title ?? "Supplied password is not acceptable"
+                }
             );
         }
 
