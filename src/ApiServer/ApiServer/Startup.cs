35b1b3a353e265e298f280859a0eaa1b12e5b78a
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2021-11-08 12:20:58 +0100

    Remove unused MySQL NuGet packages

--- src/ApiServer/ApiServer/Startup.cs
+++ src/ApiServer/ApiServer/Startup.cs
@@ -14,8 +14,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
