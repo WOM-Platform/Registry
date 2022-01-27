cc73f6225d8b2600e9b4b79c53313854c083e701
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2022-01-27 15:29:24 +0100

    Add per source voucher stats end-point

---
+++ src/ApiServer/ApiServer/OutputModels/Stats/VoucherSourceStatsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WomPlatform.Web.Api.OutputModels.Stats {
+
+    public class VoucherSourceStatsResponse {
+
+        public long TotalVouchersGenerated { get; init; }
+
+        public long TotalVouchersRedeemed { get; init; }
+
+    }
+
+}
