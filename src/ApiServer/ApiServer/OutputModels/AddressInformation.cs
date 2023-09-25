6f3140f5eb92c7e1b0fc0e07f71625333fe93a79
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2023-09-26 01:13:22 +0200

    Clean up address output

--- src/ApiServer/ApiServer/OutputModels/AddressInformation.cs
+++ src/ApiServer/ApiServer/OutputModels/AddressInformation.cs
@@ -1,4 +1,5 @@
-﻿using WomPlatform.Web.Api.DatabaseDocumentModels;
+﻿using System.Linq;
+using WomPlatform.Web.Api.DatabaseDocumentModels;
 
 namespace WomPlatform.Web.Api.OutputModels {
     public class AddressInformation {
@@ -19,7 +20,14 @@ namespace WomPlatform.Web.Api.OutputModels {
 
     public static class AddressInformationExtensions {
         public static AddressInformation ToOutput(this AddressBlock addressBlock) {
-            return addressBlock == null ? null : new AddressInformation {
+            if(addressBlock == null) {
+                return null;
+            }
+            if(!new string[] { addressBlock.StreetName, addressBlock.ZipCode, addressBlock.City, addressBlock.Country, addressBlock.FormattedAddress }.Any()) {
+                return null;
+            }
+
+            return new AddressInformation {
                 StreetName = addressBlock.StreetName,
                 StreetNumber = addressBlock.StreetNumber,
                 ZipCode = addressBlock.ZipCode,
