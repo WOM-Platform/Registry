255608dc21263a154570c2886b32ec721e93bd7c
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2024-05-10 19:05:42 +0200

    Move aims to static definitions in code

--- src/ApiServer/ApiServer/Controllers/AimsControllerV2.cs
+++ src/ApiServer/ApiServer/Controllers/AimsControllerV2.cs
@@ -1,10 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WomPlatform.Web.Api.DatabaseDocumentModels;
 using WomPlatform.Web.Api.OutputModels.Aim;
 using WomPlatform.Web.Api.Service;
 
@@ -36,8 +35,8 @@ namespace WomPlatform.Web.Api.Controllers {
         [ChangeLog("aim-list")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(AimListResponse), StatusCodes.Status200OK)]
-        public async Task<IActionResult> ListV2() {
-            var aims = await _aimService.GetAllAims();
+        public ActionResult ListFlat() {
+            var aims = _aimService.GetFlatAims();
 
             return Ok(new AimListResponse {
                 Aims = (from a in aims
@@ -45,41 +44,8 @@ namespace WomPlatform.Web.Api.Controllers {
                             Code = a.Code,
                             Titles = a.Titles,
                             Hidden = a.Hidden,
-                            Children = null,
-                        }).ToArray()
+                        }).ToArray(),
             });
-        }
-
-        private void InsertAimEntry(AimListResponse.AimNode entry, Aim aim, int level) {
-            // Insertion point is at first character "larger" than current
-            // Starts at -1, which indicates insertion at the back
-            int insertionIndex = -1;
-            for(int i = 0; i < entry.Children.Length; ++i) {
-                if(entry.Children[i].Code[level] == aim.Code[level]) {
-                    // First character matches with first character of insertion point, we must insert at a lower level
-                    InsertAimEntry(entry.Children[i], aim, level + 1);
-                    return;
-                }
-                else if(entry.Children[i].Code[level] > aim.Code[level]) {
-                    insertionIndex = i;
-                    break;
-                }
-            }
-            if(insertionIndex == -1) {
-                insertionIndex = entry.Children.Length; // Append at end
-            }
-
-            var newChildren = new AimListResponse.AimNode[entry.Children.Length + 1];
-            Array.Copy(entry.Children[..insertionIndex], newChildren, insertionIndex);
-            newChildren[insertionIndex] = new AimListResponse.AimNode {
-                Code = aim.Code,
-                Titles = aim.Titles,
-                Hidden = aim.Hidden,
-                Children = Array.Empty<AimListResponse.AimNode>(),
-            };
-            Array.Copy(entry.Children[insertionIndex..], 0, newChildren, insertionIndex + 1, entry.Children.Length - insertionIndex);
-
-            entry.Children = newChildren;
         }
 
         /// <summary>
@@ -90,16 +56,32 @@ namespace WomPlatform.Web.Api.Controllers {
         [ChangeLog("aim-list")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(AimListResponse), StatusCodes.Status200OK)]
-        public async Task<IActionResult> ListNestedV2() {
-            var aims = await _aimService.GetAllAims();
+        public ActionResult ListNested() {
+            var aims = _aimService.GetAims();
 
-            var root = new AimListResponse.AimNode { Children = Array.Empty<AimListResponse.AimNode>() };
-            foreach(var aim in aims) {
-                InsertAimEntry(root, aim, 0);
+            void RecursiveAddToList(IReadOnlyList<AimService.Aim> aims, List<AimListResponse.AimNode> target) {
+                foreach(var a in aims) {
+                    var node = new AimListResponse.AimNode {
+                        Code = a.Code,
+                        Titles = a.Titles,
+                        Hidden = a.Hidden,
+                        Children = [],
+                    };
+
+                    if(a.Aims.Length > 0) {
+                        var children = new List<AimListResponse.AimNode>();
+                        RecursiveAddToList(a.Aims, children);
+                        node.Children = children.ToArray();
+                    }
+
+                    target.Add(node);
+                }
             }
+            var output = new List<AimListResponse.AimNode>();
+            RecursiveAddToList(aims, output);
 
             return Ok(new AimListResponse {
-                Aims = root.Children
+                Aims = output.ToArray(),
             });
         }
 
--- src/ApiServer/ApiServer/Service/AimService.cs
+++ src/ApiServer/ApiServer/Service/AimService.cs
@@ -1,66 +1,142 @@
 ﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using MongoDB.Bson;
-using MongoDB.Driver;
-using WomPlatform.Web.Api.DatabaseDocumentModels;
 
 namespace WomPlatform.Web.Api.Service {
-    public class AimService : BaseService {
+    public class AimService {
 
-        public AimService(
-            MongoClient client,
-            ILogger<BaseService> logger
-        ) : base(client, logger) {
+        public class Aim {
+            public Aim(string code, Dictionary<string, string> titles, Aim[] subaims = null, bool hidden = false) {
+                Code = code;
+                Titles = titles;
+                Aims = subaims ?? [];
+                Hidden = hidden;
+            }
 
+            public string Code { get; init; }
+
+            public Dictionary<string, string> Titles { get; init; }
+
+            public Aim[] Aims { get; init; }
+
+            public bool Hidden { get; init; }
         }
+
+        private static Dictionary<string, string> Titles(string singleTitle) {
+            return new Dictionary<string, string>() {
+                ["en"] = singleTitle,
+                ["it"] = singleTitle,
+            };
+        }
+
+        private static Dictionary<string, string> Titles(string english, string italian) {
+            return new Dictionary<string, string>() {
+                ["en"] = english,
+                ["it"] = italian,
+            };
+        }
+
+        private static Aim[] Aims = [
+            new Aim("0", Titles("Demo"), hidden: true),
+            new Aim("C", Titles("Culture", "Cultura")),
+            new Aim("E", Titles("Education", "Istruzione")),
+            new Aim("H", Titles("Health and Wellbeing", "Salute e benessere"), [
+                new Aim("HE", Titles("Epidemic Containment", "Contenimento di epidemia")),
+            ]),
+            new Aim("I", Titles("Infrastructure and Services", "Infrastruttura e servizi"), [
+                new Aim("IC", Titles("Collaboration", "Collaborazione")),
+                new Aim("IG", Titles("Management", "Gestione")),
+                new Aim("IM", Titles("Infrastructure Monitoring", "Monitoraggio infrastrutturale")),
+                new Aim("IS", Titles("Service", "Servizio")),
+            ]),
+            new Aim("N", Titles("Natural Environment", "Ambiente naturale")),
+            new Aim("P", Titles("Participation", "Partecipazione")),
+            new Aim("R", Titles("Human Rights", "Diritti umani")),
+            new Aim("S", Titles("Safety", "Sicurezza")),
+            new Aim("T", Titles("Cultural Heritage", "Patrimonio culturale")),
+            new Aim("U", Titles("Urban Environment", "Ambiente urbano"), [
+                new Aim("UM", Titles("Urban Mobility", "Mobilità urbana")),
+            ]),
+            new Aim("X", Titles("Social Cohesion", "Coesione sociale"), [
+                new Aim("XX", Titles("Gratitude", "Gratitudine")),
+            ]),
+        ];
+
+        private static ImmutableDictionary<string, Aim> AimsByCode = ImmutableDictionary<string, Aim>.Empty;
+
+        private static ImmutableArray<Aim> FlatAims = [];
+
+        static AimService() {
+            // Prepare dictionary access by code
+            void RecursiveAddToDictionary(Aim[] list, Dictionary<string, Aim> target) {
+                foreach(var aim in list) {
+                    target.Add(aim.Code, aim);
+                    RecursiveAddToDictionary(aim.Aims, target);
+                }
+            }
+            Dictionary<string, Aim> aimsByCode = [];
+            RecursiveAddToDictionary(Aims, aimsByCode);
+            AimsByCode = aimsByCode.ToImmutableDictionary();
+
+            // Prepare flat list
+            void RecursiveAddToList(Aim[] list, List<Aim> target) {
+                foreach(var aim in list) {
+                    target.Add(aim);
+                    RecursiveAddToList(aim.Aims, target);
+                }
+            }
+            List<Aim> flatAims = [];
+            RecursiveAddToList(Aims, flatAims);
+            FlatAims = (from a in flatAims
+                        orderby a.Code
+                        select a).ToImmutableArray();
+        }
+
+        private readonly ILogger<BaseService> _logger;
+
+        public AimService(ILogger<BaseService> logger) {
+            _logger = logger;
+        }
+
+        protected ILogger<BaseService> Logger { get { return _logger; } }
 
         /// <summary>
         /// Get all aims sorted by code.
         /// </summary>
-        public Task<List<Aim>> GetAllAims() {
-            return AimCollection
-                .Find(Builders<Aim>.Filter.Empty)
-                .SortBy(aim => aim.Code)
-                .ToListAsync();
+        public IReadOnlyList<Aim> GetFlatAims() {
+            return FlatAims;
         }
 
         /// <summary>
         /// Get all root aims.
         /// </summary>
-        public Task<List<Aim>> GetRootAims() {
-            return AimCollection.Aggregate()
-                // Projects and adds length and root (first code character)
-                .Project<BsonDocument>((ProjectionDefinition<Aim>)@"{
-                    ""titles"": 1,
-                    ""order"": 1,
-                    ""hidden"": 1,
-                    ""length"": {$strLenCP: ""$_id""},
-                    ""root"": { $substrCP:[ ""$_id"", 0, 1 ] }
-                }")
-                // Match only aims with length == 1
-                .Match(@"{
-                    ""length"": 1
-                }")
-                // Sort by root aim
-                .Sort(@"{
-                    ""_id"": 1
-                }")
-                .As<Aim>()
-                .ToListAsync();
+        public IReadOnlyList<Aim> GetAims() {
+            return Aims.ToImmutableArray();
         }
 
         /// <summary>
         /// Get all root aim codes.
         /// </summary>
-        public async Task<string[]> GetRootAimCodes() {
-            return (from a in await GetRootAims() select a.Code).ToArray();
+        public string[] GetRootAimCodes() {
+            return (from a in Aims
+                    orderby a.Code
+                    select a.Code).ToArray();
         }
 
-        public Task<Aim> GetAimByCode(string code) {
-            var filter = Builders<Aim>.Filter.Eq(a => a.Code, code);
-            return AimCollection.Find(filter).SingleOrDefaultAsync();
+        /// <summary>
+        /// Get all aim codes.
+        /// </summary>
+        public string[] GetAllAimCodes() {
+            return (from a in FlatAims
+                    select a.Code).ToArray();
+        }
+
+        /// <summary>
+        /// Get aim by unique code.
+        /// </summary>
+        public Aim GetAimByCode(string code) {
+            return AimsByCode.TryGetValue(code, out Aim value) ? value : null;
         }
 
     }
