aae001c3bedcec23d9d8788d6391cdf4b225fe8c
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2025-05-23 22:41:47 +0200

    Add document models

---
+++ src/ApiServer/ApiServer/DatabaseDocumentModels/Badge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace WomPlatform.Web.Api.DatabaseDocumentModels {
+    public class Badge {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("challengeId")]
+        [BsonIgnoreIfNull]
+        public ObjectId ChallengeId { get; set; }
+
+        [BsonElement("isPublic")]
+        public bool IsPublic { get; set; }
+
+        [BsonElement("name")]
+        public Dictionary<string, string> Name { get; set; }
+
+        [BsonElement("simpleFilter")]
+        [BsonIgnoreIfNull]
+        public BadgeSimpleFilter? SimpleFilter { get; set; }
+
+        [BsonElement("imagePath")]
+        public string ImagePath { get; set; }
+
+        [BsonElement("imageBlurHash")]
+        public string ImageBlurHash { get; set; }
+
+        [BsonElement("description")]
+        public Dictionary<string, string> Description { get; set; }
+
+        [BsonElement("informationUrl")]
+        [BsonIgnoreIfNull]
+        public string? InformationUrl { get; set; }
+
+        [BsonElement("createdAt")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonElement("lastUpdate")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonIgnoreIfNull]
+        public DateTime? LastUpdate { get; set; }
+
+        [BsonExtraElements]
+        public BsonDocument CatchAll { get; set; }
+    }
+}
---
+++ src/ApiServer/ApiServer/DatabaseDocumentModels/BadgeSimpleFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WomPlatform.Web.Api.DatabaseDocumentModels {
+    public class BadgeSimpleFilter {
+        [BsonElement("count")]
+        public int Count { get; set; }
+
+        [BsonElement("aim")]
+        [BsonIgnoreIfDefault]
+        public string? Aim { get; set; }
+
+        [BsonElement("interval")]
+        [BsonIgnoreIfNull]
+        public IntervalSpecification? Interval { get; set; }
+
+        public class IntervalSpecification {
+            [BsonElement("start")]
+            public DateTime Start { get; set; }
+
+            [BsonElement("end")]
+            public DateTime End { get; set; }
+        }
+    }
+}
